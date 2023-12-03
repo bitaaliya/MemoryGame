0ca2a6de699346cabd0870953e9c5952c1c71126
Lalu Usamah Wahidil Qahar <105252850+LaluUsamahWahidilQahar@users.noreply.github.com>
2023-12-03 14:31:02 +0800

    Add files via upload
    test upload

---
+++ MemoryGame/src/main/java/com/mycompany/memorygame/Controler/CountDown.java
@@ -0,0 +1,50 @@
+package com.mycompany.memorygame.Controler;
+
+import com.mycompany.memorygame.GameFrame;
+
+public class CountDown extends Thread {
+    private int time;
+    private boolean isruning = true;       
+    private Match matchControler;
+    private GameFrame gameFrame;
+
+    public CountDown(int time, Match matchControler, GameFrame gameFrame) {
+        this.time = time;
+        this.matchControler = matchControler;
+        this.gameFrame = gameFrame;
+    
+    }
+
+    public void run(){
+        
+
+        while (isruning && time > 0) {
+           
+            try {
+                CountDown.sleep(1000);
+                time--;
+                gameFrame.setShowTime(String.format("%02d:%02d",0, time));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+        }
+        if (time == 0) {
+            matchControler.end();
+            gameFrame.setShowTime("00:00");
+
+            
+        }
+        else if (time<0) {
+            gameFrame.setShowTime("01:00");
+
+        }
+    }
+
+    public void interrupt(){
+        isruning = false;
+        time=0;
+        gameFrame.setShowTime("01:00");
+        
+    }
+}
---
+++ MemoryGame/src/main/java/com/mycompany/memorygame/Controler/Match.java
@@ -0,0 +1,7 @@
+package com.mycompany.memorygame.Controler;
+
+public class Match {
+    public void end(){
+        
+    }
+}
