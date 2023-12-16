dc1e6efa96023b2f7c487fe1fe5ae812df62dd00
Lalu Usamah Wahidil Qahar <105252850+LaluUsamahWahidilQahar@users.noreply.github.com>
2023-12-16 22:36:40 +0800

    abstarc and interface

---
+++ MemoryGame/src/main/java/com/mycompany/memorygame/Controler/match.java
@@ -0,0 +1,35 @@
+package com.mycompany.memorygame.Controler;
+
+import javax.swing.JButton;
+
+public abstract class match {
+
+
+
+    public match() {
+
+        
+    }
+
+
+
+
+    public void setNumberBtn(int numberBtn) {
+        
+    }
+
+ 
+
+    public void setBtn(JButton btn) {
+        
+    }
+
+  
+
+
+    public void setValueMatch() {
+        
+    }
+
+
+}
